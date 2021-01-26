58c80477f8d208ca92b4ca58b896d4257fd31cb6
arunxavy <arunxavy@gmail.com>
2021-01-26 13:20:04 +0530

    Update colorize.js
    updated

--- colorize.js
+++ colorize.js
@@ -1,11 +1,41 @@
 deepai.setApiKey(atob('NWFhZmU4MTQtY2JjNy00MjU4LWE5NGEtM2E5OWVlNDBmNDBh'));
 async function colorize() {
-document.getElementById("msg").style.display = 'block';
-	var resp = await deepai.callStandardApi("colorizer", {
+		var resp = await deepai.callStandardApi("colorizer", {
 		image: document.getElementById("url").value,
 	});
-	document.getElementById("msg").style.display = 'none';
 	console.log(resp);
-	await deepai.renderResultIntoElement(resp, document.getElementById('resp'));
+	//await deepai.renderResultIntoElement(resp, document.getElementById('output_img'));
+	document.getElementById("output_img").src = resp.output_url;
+	download(resp.output_url);
+}
 
+function loadImage() {
+
+	var image = document.createElement("img");
+	var imageParent = document.getElementById("ipGrid");
+	image.id = "input_img";
+	image.className = "class";
+	image.src = document.getElementById("url").value;
+	imageParent.appendChild(image);
+
+	var opImg = document.createElement("img");
+	var opParent = document.getElementById("opGrid");
+	opImg.id = "output_img";
+	opImg.className = "class";
+	opImg.src = null;
+	opImg.alt = 'Image loading';
+	opParent.appendChild(opImg);
+	colorize();
+}
+
+function download(url){
+var btnParent = document.getElementById("buttons");
+var anchor = document.createElement('a');
+anchor.href = url;
+anchor.download = url;
+anchor.target = '_blank';
+var linkText = document.createTextNode("Download");
+anchor.appendChild(linkText);
+btnParent.appendChild(anchor);
+console.log(url);
 }
