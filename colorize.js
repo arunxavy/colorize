3c3205aea900d87c9e4e4d36ef941d55121483ee
arunxavy <arunxavy@gmail.com>
2021-01-27 21:50:44 +0530

    Update colorize.js

--- colorize.js
+++ colorize.js
@@ -1,17 +1,21 @@
 deepai.setApiKey(atob('NWFhZmU4MTQtY2JjNy00MjU4LWE5NGEtM2E5OWVlNDBmNDBh'));
 async function colorize() {
-		var resp = await deepai.callStandardApi("colorizer", {
+
+	var resp = await deepai.callStandardApi("colorizer", {
 		image: document.getElementById("url").value,
 	});
 	download(resp.output_url);
 	document.getElementById("output_img").src = resp.output_url;
-	
+
 }
 
 function loadImage() {
 
 	var image = document.createElement("img");
 	var imageParent = document.getElementById("ipGrid");
+	if(imageParent.childNodes[0]){
+	imageParent.removeChild(imageParent.childNodes[0]);
+	}
 	image.id = "input_img";
 	image.className = "class";
 	image.src = document.getElementById("url").value;
@@ -19,6 +23,9 @@ function loadImage() {
 
 	var opImg = document.createElement("img");
 	var opParent = document.getElementById("opGrid");
+	if(opParent.childNodes[0]){
+	opParent.removeChild(opParent.childNodes[0]);
+	}
 	opImg.id = "output_img";
 	opImg.className = "class";
 	opImg.src = null;
@@ -27,33 +34,39 @@ function loadImage() {
 	colorize();
 }
 
-function download(url){
+function download(url) {
 
-toDataURL(url, function(dataUrl) {
-  	var arr = url.split("/");
-  	var btnParent = document.getElementById("buttons");
-	var anchor = document.createElement('a');
-	anchor.href = dataUrl;
-	anchor.download = arr[arr.length-1];
-	anchor.target = '_blank';
-	var linkText = document.createTextNode("Download");
-	anchor.appendChild(linkText);
-	btnParent.appendChild(anchor);
-})
+	toDataURL(url, function(dataUrl) {
+		var arr = url.split("/");
+		var btnParent = document.getElementById("buttons");
+		
+		var alreadyCreatedDwBtn = document.getElementById('dwBtn');
+		if(alreadyCreatedDwBtn){
+		alreadyCreatedDwBtn.parentNode.removeChild(alreadyCreatedDwBtn);
+		}
+		var anchor = document.createElement('a');
+		anchor.href = dataUrl;
+		anchor.id='dwBtn';
+		anchor.download = arr[arr.length - 1];
+		anchor.target = '_blank';
+		var linkText = document.createTextNode("Download");
+		anchor.appendChild(linkText);
+		btnParent.appendChild(anchor);
+	})
 
-console.log(url);
+	console.log(url);
 }
 
 function toDataURL(url, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function() {
-    var reader = new FileReader();
-    reader.onloadend = function() {
-      callback(reader.result);
-    }
-    reader.readAsDataURL(xhr.response);
-  };
-  xhr.open('GET', url);
-  xhr.responseType = 'blob';
-  xhr.send();
+	var xhr = new XMLHttpRequest();
+	xhr.onload = function() {
+		var reader = new FileReader();
+		reader.onloadend = function() {
+			callback(reader.result);
+		}
+		reader.readAsDataURL(xhr.response);
+	};
+	xhr.open('GET', url);
+	xhr.responseType = 'blob';
+	xhr.send();
 }
